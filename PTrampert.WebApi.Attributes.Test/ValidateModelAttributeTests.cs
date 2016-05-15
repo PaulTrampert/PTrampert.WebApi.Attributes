0882f9002c026bd7f7159401faa76bbca5a672a8
Paul Trampert <paul.trampert@gmail.com>
2016-05-15 16:50:41 -0400

    Added ValidateModelAttribute.

---
+++ PTrampert.WebApi.Attributes.Test/ValidateModelAttributeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+using NUnit.Framework;
+
+namespace PTrampert.WebApi.Attributes.Test
+{
+    [TestFixture]
+    public class ValidateModelAttributeTests
+    {
+        private ValidateModelAttribute Attribute { get; set; }
+
+        private HttpActionContext Context { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Attribute = new ValidateModelAttribute
+            {
+                TestRequest = new HttpRequestMessage(),
+            };
+            Context = new HttpActionContext();
+        }
+
+        [Test]
+        public void ResponseIsNotSetWhenModelStateIsValid()
+        {
+            Attribute.OnActionExecuting(Context);
+            Assert.That(Context.Response, Is.Null);
+        }
+
+        [Test]
+        public void BadRequestResponseIsReturnedWhenModelStateIsNotValid()
+        {
+            Context.ModelState.AddModelError("Property", "Something was bad");
+            Attribute.OnActionExecuting(Context);
+            Assert.That(Context.Response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+    }
+}
---
+++ PTrampert.WebApi.Attributes/ValidateModelAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace PTrampert.WebApi.Attributes
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public HttpRequestMessage TestRequest { get; set; }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            ValidateModel(actionContext, TestRequest ?? actionContext.Request);
+        }
+
+        private static void ValidateModel(HttpActionContext actionContext, HttpRequestMessage request)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Response = request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                    actionContext.ModelState);
+            }
+        }
+    }
+}
