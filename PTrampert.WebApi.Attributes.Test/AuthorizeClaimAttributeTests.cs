c4795f151a274f5ab49ff17c494e820c409b45ab
Paul Trampert <paul.trampert@gmail.com>
2016-05-22 19:24:20 -0400

    Added AuthorizeClaimAttribute.

---
+++ PTrampert.WebApi.Attributes.Test/AuthorizeClaimAttributeTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using NUnit.Framework;
+
+namespace PTrampert.WebApi.Attributes.Test
+{
+    [TestFixture]
+    public class AuthorizeClaimAttributeTests
+    {
+
+        private AuthorizeClaimAttribute Attribute { get; set; }
+
+        private HttpActionContext Context { get; set; }
+
+        private ClaimsIdentity Identity { get; set; }
+
+        private HttpRequestMessage Request { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Attribute = new AuthorizeClaimAttribute("key", "value");
+            Context = new HttpActionContext();
+            Request = new HttpRequestMessage();
+            Identity = new ClaimsIdentity(new List<Claim> {new Claim("key", "value")}, "Basic");
+        }
+
+        [Test]
+        public void WhenUserHasClaimResponseIsNotSet()
+        {
+            Attribute.AuthorizeClaim(Context, Identity, Request);
+            Assert.That(Context.Response, Is.Null);
+        }
+
+        [Test]
+        public void WhenUserDoesntHaveClaimResponseIsForbidden()
+        {
+            Attribute.AuthorizeClaim(Context, new ClaimsIdentity(new List<Claim>(), "Basic"), Request);
+            Assert.That(Context.Response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+        }
+
+        [Test]
+        public void WhenUserIsUnauthenticatedResponseIsUnauthorized()
+        {
+            Attribute.AuthorizeClaim(Context, new ClaimsIdentity(), Request);
+            Assert.That(Context.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public void WhenUserIsNullResponseIsUnauthorized()
+        {
+            Attribute.AuthorizeClaim(Context, null, Request);
+            Assert.That(Context.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+    }
+}
---
+++ PTrampert.WebApi.Attributes/AuthorizeClaimAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace PTrampert.WebApi.Attributes
+{
+    public class AuthorizeClaimAttribute : AuthorizeAttribute
+    {
+        private readonly string _key;
+        private readonly string _value;
+
+        public AuthorizeClaimAttribute(string key, string value)
+        {
+            _key = key;
+            _value = value;
+        }
+
+        public HttpRequestMessage TestRequest { get; set; }
+
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            base.OnAuthorization(actionContext);
+            AuthorizeClaim(actionContext, actionContext.RequestContext.Principal.Identity as ClaimsIdentity, actionContext.Request);
+        }
+
+        public void AuthorizeClaim(HttpActionContext actionContext, ClaimsIdentity identity, HttpRequestMessage request)
+        {
+            if (identity == null || !identity.IsAuthenticated)
+            {
+                actionContext.Response = request.CreateResponse(HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            if (!identity.HasClaim(_key, _value))
+                actionContext.Response = request.CreateResponse(HttpStatusCode.Forbidden);
+        }
+    }
+}
